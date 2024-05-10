10344731ec6917d2422da83a14b95c5d7dc689a7
jesskabasilio <jessica.limabasilio@gmail.com>
2024-05-10 14:50:28 -0300

    updating sales, selecting values on table

--- aula2.sql
+++ aula2.sql
@@ -17,3 +17,21 @@ CREATE TABLE venda(
 	id_usuario INT,
 	valor DECIMAL (9,2)
 )
+
+SELECT * FROM usuarios
+
+--adicionando vendas--
+
+INSERT INTO venda (id_usuario,valor) VALUES (1, 500.00)
+INSERT INTO venda (id_usuario,valor) VALUES (2, 300.00)
+INSERT INTO venda (id_usuario,valor) VALUES (3, 700.00)
+
+SELECT * FROM venda
+
+--adicionando na tabela venda os dados do usuário--
+
+SELECT * FROM venda INNER JOIN usuarios ON venda.id_usuario = usuarios.id
+
+--Trocando o código do usuário pelo nome do usuário e seleionando o que quer mostrar na tabela--
+
+SELECT venda.cod, venda.data, usuarios.nome, venda.valor FROM venda INNER JOIN usuarios ON venda.id_usuario = usuarios.id
