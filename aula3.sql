b2d675260ae5953c28a9f0bd05ed77b04bc052aa
jesskabasilio <jessica.limabasilio@gmail.com>
2024-05-10 22:22:35 -0300

    updating Fk's

--- aula3.sql
+++ aula3.sql
@@ -27,10 +27,24 @@ drop table funcionario
 --inserindo dados dentros das tablelas setor, cargo e funcionário--
 
 INSERT INTO setor (nome) VALUES ('Recursos Humanos')
-SELECT * FROM setor
+
 
 INSERT INTO cargo (titulo, id_setor) VALUES ('Secretaria', 1)
-SELECT * FROM cargo
+
 
 INSERT INTO funcionario (nome, id_cargo) VALUES ('Maria Andrade', 1)
 SELECT * FROM funcionario
+
+SELECT * FROM setor
+SELECT * FROM cargo
+SELECT * FROM funcionario
+
+/*Foi acrescentado a chave estrangeira dentro das tabelas, modificado dentro do programa e ela
+existe para que o usuário não cometa erros, até o exato momento
+temos apenas uma inserção em cada tabela, veja a tentativa abaixo:*/
+
+INSERT INTO funcionario (nome, id_cargo) VALUES ('João Andrade', 2) /*não dá certo, pois ainda não foi criado o cargo 2,
+se trocar para cargo 1, que existe, o funcionarios será criado na tabela. Essa é a vantagem da criação da chave estrangeira*/
+
+INSERT INTO funcionario (nome, id_cargo) VALUES ('João Andrade', 1) /*funcionario criado com sucesso*/
+
